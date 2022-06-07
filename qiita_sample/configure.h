bd8c9e30148bff20821eff9dc245a851d62d46a9
Shogo Kohraku <skohraku@gmail.com>
2022-06-07 11:53:09 +0900

    add: qiitaの「Webサーバの動作を理解するための〜」のコードを追加
    記載コードからindex.htmlが表示されるように修正しました。

---
+++ qiita_sample/configure.h
@@ -0,0 +1,9 @@
+#ifndef _CONFHET_H_
+#define _CONFHET_H_
+
+const int BUF_SIZE = 3000;//1024;
+const int HTTP1_PORT = 5000;
+const int HTTP_VERSION = 1;
+const std::string HTML_FILE = "www";
+
+#endif
