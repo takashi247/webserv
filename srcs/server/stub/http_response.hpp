0383fd33d8accfa4610df12efdedbc6505c0c7bb
Shogo Kohraku <skohraku@gmail.com>
2022-06-16 09:23:58 +0900

    fix: コミット漏れファイルの追加

---
+++ srcs/server/stub/http_response.hpp
@@ -0,0 +1,36 @@
+#ifndef HTTP_RESPONSE_HPP
+#define HTTP_RESPONSE_HPP
+
+#include <string>
+
+#include "config.hpp"
+#include "http_request.hpp"
+
+class HttpResponse {
+ private:
+  HttpResponse();
+
+ public:
+  HttpResponse(const HttpRequest* request, const ServerConfig* config) {
+    (void)request;
+    (void)config;
+  }
+  ~HttpResponse() {}
+  std::string GetResponse() {
+    std::string server_response;
+    std::vector<std::string> header;
+    header.push_back("HTTP/1.1 200 OK\r\n");
+    header.push_back("Content-Type: text/html; charset=UTF-8\r\n");
+    header.push_back("Content-Length: 39\r\n");
+    header.push_back("Connection: Keep-Alive\r\n");
+    header.push_back("\r\n");
+    header.push_back("<html><body>Hello,world!</body></html>\r\n");
+    int header_size = header.size();
+    for (int i = 0; i < header_size; i++) {
+      server_response.append(header[i].c_str());
+    }
+    return server_response;
+  }
+};
+
+#endif
