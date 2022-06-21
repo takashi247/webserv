47976b7508e28e7da20e5e35b552d014bbfb151f
fyuta <fyuta@student.42tokyo.jp>
2022-06-22 00:24:48 +0900

    format, remove comment out

--- srcs/config/server_config.hpp
+++ srcs/config/server_config.hpp
@@ -1,13 +1,14 @@
 #ifndef SERVER_CONFIG_HPP_
-# define SERVER_CONFIG_HPP_
+#define SERVER_CONFIG_HPP_
 
-#include <set>
-#include <string>
-#include <vector>
+#include <sys/stat.h>
+
 #include <algorithm>
 #include <iostream>
+#include <set>
 #include <sstream>
-#include <sys/stat.h>
+#include <string>
+#include <vector>
 
 #include "location_config.hpp"
 #include "parser_utils.hpp"
@@ -21,7 +22,7 @@ class ServerConfig {
   ServerConfig &operator=(const ServerConfig &rhs);
 
   // URIとlocation_path_の前方一致で一番長いものを返す、見つからない場合はNULLは返さず、defaultの値が使われることにする。
-  LocationConfig *SelectLocationConfig(const std::string& uri);
+  LocationConfig *SelectLocationConfig(const std::string &uri);
   // テスト参照、返したパスにファイルが存在するかどうかは別で確認の必要があり。
   std::string UpdateUri(std::string uri);
   void PrintVal();
@@ -34,7 +35,6 @@ class ServerConfig {
   std::string error_page_path_;
   size_t client_max_body_size_;
   std::vector<LocationConfig> vec_location_config_;
-  // LocationConfig default_location_config_;
  private:
   void Init();
 };
