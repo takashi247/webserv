f95a3d7400ed61f7b48a79715f54f1c441be66cb
takashi247 <takashi247@gmail.com>
2022-06-22 15:49:18 +0900

    delete vec_test.cpp

--- vec_test.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-#include <vector>
-
-int main() {
-  std::vector< int > vec;
-  for (std::vector< int >::iterator it = vec.begin(); it != vec.end(); ++it) {
-    std::cout << *it << std::endl;
-  }
-}
