87608b03920e4f57d5c55a1de5e6e20f814e541c
fyuta <fyuta@student.42tokyo.jp>
2022-06-28 00:52:56 +0900

    add test: autoindex, illigal location

--- test/request_test.py
+++ test/request_test.py
@@ -5,14 +5,14 @@ import unittest
 class TestRequest(unittest.TestCase):
     def send_request(self, uri: str) -> requests.Response:
         try:
-            response = requests.get(uri, timeout=2.0)
+            response = requests.get(uri, timeout=2.0, allow_redirects=False)
         except Exception as e:
             print(e)
             self.fail()
         return (response)
 
     def get_autoindex_titile(self, text: str) -> str:
-        return ''.join(text.split('\n')[:2])
+        return ''.join(text.splitlines()[:2])
 
     def test_normal(self):
         got = self.send_request("http://127.0.0.1:8080/2.html")
@@ -20,12 +20,12 @@ class TestRequest(unittest.TestCase):
         self.assertEqual(got.text, "2\n")
 
     def test_index_1(self):
-        got = self.send_request("http://127.0.0.1:8081")
+        got = self.send_request("http://127.0.0.1:8080")
         self.assertEqual(got.status_code, 200)
         self.assertEqual(got.text, "1\n")
 
     def test_index_2(self):
-        got = self.send_request("http://127.0.0.1:8081/")
+        got = self.send_request("http://127.0.0.1:8080/")
         self.assertEqual(got.status_code, 200)
         self.assertEqual(got.text, "1\n")
 
@@ -33,19 +33,20 @@ class TestRequest(unittest.TestCase):
         got = self.send_request("http://localhost:8081/autoindex/")
         self.assertEqual(got.status_code, 200)
         self.assertEqual(self.get_autoindex_titile(got.text), "<html><head><title>Index of /autoindex/</title></head>")
-    #
 
     def test_autoindex_2(self):
         got = self.send_request("http://localhost:8081/autoindex")
-        self.assertEqual(got.status_code, 200)
-        self.assertEqual(self.get_autoindex_titile(got.text), "<html><head><title>Index of /autoindex</title></head>")
-    # Index of /autoindex/ ??
+        self.assertEqual(got.status_code, 301)
+    # config
 
     def test_autoindex_3(self):
-        got = self.send_request("http://localhost:8081/autoindex/inner")
+        got = self.send_request("http://localhost:8081/autoindex/inner/")
         self.assertEqual(got.status_code, 200)
-        self.assertEqual(self.get_autoindex_titile(got.text), "<html><head><title>Index of /autoindex/inner</title></head>")
-    # [TODO] compare with nginx
+        self.assertEqual(self.get_autoindex_titile(got.text), "<html><head><title>Index of /autoindex/inner/</title></head>")
+
+    def test_autoindex_4(self):
+        got = self.send_request("http://localhost:8081/autoindex/inner")
+        self.assertEqual(got.status_code, 301)
 
     def test_nosuch_file(self):
         got = self.send_request("http://localhost:8081/nosuch.txt")
@@ -55,18 +56,30 @@ class TestRequest(unittest.TestCase):
         got = self.send_request("http://localhost:8081/nosuchdir/")
         self.assertEqual(got.status_code, 404)
 
+    def test_illigal_location_1(self):
+        got = self.send_request("http://localhost:8081/cgi-bin/")
+        self.assertEqual(got.status_code, 200)
+        self.assertEqual(got.text, "index\n")
+    # index 要調査
+
+    def test_illigal_location_2(self):
+        got = self.send_request("http://localhost:8081/cgi-bin/index.html")
+        self.assertEqual(got.status_code, 200)
+        self.assertEqual(got.text, "index\n")
+    # 要調査
+
     # def test_cgi_cgi(self):
     #     got = self.send_request("http://localhost:8082/cgi-bin/hello-perl.cgi")
     #     self.assertEqual(got.status_code, 200)
     #     self.assertEqual(got.text, "hello\n")
 
     # def test_cgi_py(self):
-    #     got = self.send_request("http://localhost:8082/cgi/hello-perl.cgi")
+    #     got = self.send_request("http://localhost:8082/cgi-bin/hello-perl.cgi")
     #     self.assertEqual(got.status_code, 200)
     #     self.assertEqual(got.text, "hello\n")
 
     # def test_cgi_pl(self):
-    #     got = self.send_request("http://localhost:8082/cgi/hello.pl")
+    #     got = self.send_request("http://localhost:8082/cgi-bin/hello.pl")
     #     self.assertEqual(got.status_code, 200)
     #     self.assertEqual(got.text, "hello\n")
 
