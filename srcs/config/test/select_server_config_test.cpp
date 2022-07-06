87eb5f7746f82cabb66b5bd3cbaa9e46b09f23e1
fyuta <fyuta@student.42tokyo.jp>
2022-07-06 12:34:19 +0900

    update select_server_config test

--- srcs/config/test/select_server_config_test.cpp
+++ srcs/config/test/select_server_config_test.cpp
@@ -13,26 +13,26 @@ int main()
 
 	sc1.port_ = port;
 	sc1.vec_server_names_.push_back("localhost");
-	sc1.error_page_path_ = "1";
+	sc1.map_error_page_path_.insert(std::pair<int, std::string>(200, "1"));
 
 	sc2.port_ = port;
-	sc2.error_page_path_ = "2";
+	sc2.map_error_page_path_.insert(std::pair<int, std::string>(200, "2"));
 
-	sc3.error_page_path_ = "3";
+	sc3.map_error_page_path_.insert(std::pair<int, std::string>(200, "3"));
 
 	config.vec_server_config_.push_back(sc1);
 	config.vec_server_config_.push_back(sc2);
 	config.vec_server_config_.push_back(sc3);
 
-	ServerConfig *res1 = config.SelectServerConfig("", port, "localhost");
-	std::cout << res1->error_page_path_ << std::endl;
+	const ServerConfig *res1 = config.SelectServerConfig("", port, "localhost");
+	std::cout << res1->map_error_page_path_.at(200) << std::endl;
 	// expecting 1
 
-	ServerConfig *res2 = config.SelectServerConfig("", port, "127.0.0.1");
-	std::cout << res2->error_page_path_ << std::endl;
+	const ServerConfig *res2 = config.SelectServerConfig("", port, "127.0.0.1");
+	std::cout << res2->map_error_page_path_.at(200) << std::endl;
 	// expecting 2
 
-	ServerConfig *res3 = config.SelectServerConfig("", port + 2, "127.0.0.1");
-	std::cout << res3->error_page_path_ << std::endl;
+	const ServerConfig *res3 = config.SelectServerConfig("", port + 2, "127.0.0.1");
+	std::cout << res3->map_error_page_path_.at(200) << std::endl;
 	// expecting 1
 }
