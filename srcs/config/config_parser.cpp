2c3c3083a4976c7403c494f01e2f711d641d3981
fyuta <fyuta@student.42tokyo.jp>
2022-07-25 16:39:48 +0900

    fix: accept https in rewrite

--- srcs/config/config_parser.cpp
+++ srcs/config/config_parser.cpp
@@ -167,8 +167,10 @@ void ConfigParser::ParseServerConfig(
       } else if (item == "rewrite") {
         ParserUtils::ParseString(list, sc.default_location_config_.rewrite_);
         if (sc.default_location_config_.rewrite_[0] != '/' &&
-            sc.default_location_config_.rewrite_.find("http://") ==
-                std::string::npos)
+            (sc.default_location_config_.rewrite_.find("http://") ==
+                 std::string::npos ||
+             sc.default_location_config_.rewrite_.find("https://") ==
+                 std::string::npos))
           ParserUtils::MakeUnexpected(
               "redirect to non-URL in \"rewrite\" directive",
               tokens_[index_ - list.size() + 1].first);
@@ -227,7 +229,8 @@ void ConfigParser::ParseLocationConfig(
     } else if (item == "rewrite") {
       ParserUtils::ParseString(list, lc.rewrite_);
       if (lc.rewrite_[0] != '/' &&
-          lc.rewrite_.find("http://") == std::string::npos)
+          (lc.rewrite_.find("http://") == std::string::npos ||
+           lc.rewrite_.find("https://") == std::string::npos))
         ParserUtils::MakeUnexpected(
             "redirect to non-URL in \"rewrite\" directive",
             tokens_[index_ - list.size() + 1].first);
